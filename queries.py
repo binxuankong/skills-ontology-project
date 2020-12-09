fcbea528953916e47381009fd27a46a81514ec53
Bin Xuan Kong <binxuankong@gmail.com>
2020-12-09 10:15:17 +0800

    Indeed insights

--- queries.py
+++ queries.py
@@ -2,3 +2,9 @@ skills_query = """
 select s."Name" 
 from "Skills" s
 """
+
+jobs_query = """
+select Job_Title, Company, Salary, Location, Date_Posted, Description, Job_URL, Keywords_Present, Title_Keywords
+from {} j
+where j.Country = 'Malaysia'
+"""
